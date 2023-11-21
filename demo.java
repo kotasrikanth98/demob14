eeb8d75d35162e5f8dc8a1420d9b8a274f8cb299
kotasrikanth98 <kotasrikanth98@gmail.com>
2023-11-21 12:00:21 +0530

    Update demo.java
    updated sum as diff

--- demo.java
+++ demo.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class demo {
     public static void main(String[] args) {
 
-        int num1, num2, sum;
+        int num1, num2, diff;
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter First Number: ");
             num1 = sc.nextInt();
 
             System.out.println("Enter Second Number: ");
             num2 = sc.nextInt();
-            sum = num1 + num2;
-            System.out.println("Sum of these numbers: " + sum);
+        diff = num1 - num2;
+            System.out.println("Sum of these numbers: " + diff);
         }
     }
 }
