3d05e6a1763b556aad6eccc370915184a672ea9a
kotasrikanth98 <kotasrikanth98@gmail.com>
2023-11-21 11:33:26 +0530

    sum

---
+++ demo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class demo {
+    public static void main(String[] args) {
+
+        int num1, num2, sum;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter First Number: ");
+            num1 = sc.nextInt();
+
+            System.out.println("Enter Second Number: ");
+            num2 = sc.nextInt();
+            sum = num1 + num2;
+            System.out.println("Sum of these numbers: " + sum);
+        }
+    }
+}
